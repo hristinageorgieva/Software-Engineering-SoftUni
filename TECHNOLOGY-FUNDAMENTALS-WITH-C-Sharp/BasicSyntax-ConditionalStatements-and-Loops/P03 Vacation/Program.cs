3634d4dc7c3fa3c987f6e557a60a54870586faa1
Hristina <georgieva.h.g@gmail.com>
2019-08-30 20:31:59 +0300

    Added projects

---
+++ TECHNOLOGY-FUNDAMENTALS-WITH-C-Sharp/BasicSyntax-ConditionalStatements-and-Loops/P03 Vacation/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace P03Vacation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            string type = Console.ReadLine();
+            string day = Console.ReadLine();
+            double price = 0;
+            if (type == "Students")
+            {
+                switch (day)
+                {
+                    case "Friday":
+                        {
+                            price = 8.45;
+                            break;
+                        }
+                    case "Saturday":
+                        {
+                            price = 9.80;
+                            break;
+                        }
+                    case "Sunday":
+                        {
+                            price = 10.46;
+                            break;
+                        }
+                }
+                price *= count;
+                if (count >= 30)
+                {
+                    price *= 0.85;
+                }
+            }
+            if (type == "Business")
+            {
+                switch (day)
+                {
+                    case "Friday":
+                        {
+                            price = 10.90;
+                            break;
+                        }
+                    case "Saturday":
+                        {
+                            price = 15.60;
+                            break;
+                        }
+                    case "Sunday":
+                        {
+                            price = 16;
+                            break;
+                        }
+                }
+                if (count >= 100)
+                {
+                    count -= 10;
+                }
+                price *= count;
+            }
+            if (type == "Regular")
+            {
+                switch (day)
+                {
+                    case "Friday":
+                        {
+                            price = 15;
+                            break;
+                        }
+                    case "Saturday":
+                        {
+                            price = 20;
+                            break;
+                        }
+                    case "Sunday":
+                        {
+                            price = 22.50;
+                            break;
+                        }
+                }
+                price *= count;
+                if (count >= 10 && count <= 20)
+                {
+                    price *= 0.95;
+                }
+            }
+
+            Console.WriteLine($"Total price: {price:f2}");
+        }
+    }
+}
---
+++ TECHNOLOGY-FUNDAMENTALS-WITH-C-Sharp/BasicSyntax-ConditionalStatements-and-Loops/P09Padawan Equipment/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace P09PadawanEquipment
+{
+    class Program
+    {
+        static void Main()
+        {
+            decimal moneyAmount = decimal.Parse(Console.ReadLine());
+            int studentsCount = int.Parse(Console.ReadLine());
+            decimal lightsabersPrice = decimal.Parse(Console.ReadLine());
+            decimal robePrice = decimal.Parse(Console.ReadLine());
+            decimal beltsPrice = decimal.Parse(Console.ReadLine());
+
+            int beltsCount = studentsCount - (studentsCount / 6);
+            int lightsabersCount = studentsCount + (int)Math.Ceiling((studentsCount * 0.1m));
+
+            decimal moneyNeeded = lightsabersPrice * lightsabersCount +
+                robePrice * studentsCount +
+                beltsPrice * beltsCount;
+
+            decimal difference = Math.Abs(moneyAmount - moneyNeeded);
+
+            if (moneyAmount >= moneyNeeded)
+            {
+                Console.WriteLine($"The money is enough - it would cost {moneyNeeded:f2}lv.");
+            }
+            else
+            {
+                Console.WriteLine($"Ivan Cho will need {difference:f2}lv more.");
+            }
+        }
+    }
+}
